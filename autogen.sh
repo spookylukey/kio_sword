6802a53ef28122cedbaa545b9b61127acb607b51
luke <luke@localhost>
2004-06-01 17:00:30 +0000

    make the autogen.sh script work w.r.t. installing docs

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# First create all the .html files from our
+# .docbook file.  The KDE build process will
+# then create makefiles that install these docs
 cd doc/en
-meinproc
+meinproc index.docbook
 cd ../..
 WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -f Makefile.cvs
