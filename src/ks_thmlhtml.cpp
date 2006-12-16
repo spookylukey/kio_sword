b2d925d79ab2eafb1b232f0c2ca80764f49d1bbb
luke <luke@localhost>
2006-12-16 15:13:33 +0000

    - Standardised on 'Kio-Sword' for publically visible name - Made 'propagating options' propagate through the search form

--- src/ks_thmlhtml.cpp
+++ src/ks_thmlhtml.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
     File:         ks_thmlhtml.cpp
-    Project:      kio-sword -- An ioslave for SWORD and KDE
+    Project:      Kio-Sword -- An ioslave for SWORD and KDE
     Copyright:    Copyright (C) 2004-2005 Luke Plant
                   and CrossWire Bible Society 2001
                   (file based on thmlhtmlhref.cpp)
